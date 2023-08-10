363ce7aa889ffa8ec5412b5526c0ceaf273d3ec9
riemann <aleqvids@gmail.com>
2023-08-10 15:46:54 +0300

    updates

--- src/commands/setUpProfile.ts
+++ src/commands/setUpProfile.ts
@@ -7,7 +7,7 @@ class Polls {
     async sendGenderPoll(ctx: Context, userId: number) {
         const poll = await ctx.telegram.sendPoll(userId, 'Your Gender', ['Male', 'Female'], { is_anonymous: false });
 
-        if (poll && ctx.from) {
+        if (poll) {
             this.pollsMap.set(poll.poll.id, {type: 'Your Gender', userId: userId});
         }
 
@@ -17,8 +17,13 @@ class Polls {
     async sendPreferencePoll(ctx: Context, userId: number) {
         const poll = await ctx.telegram.sendPoll(userId, 'Your preference (who you want to meet)', ['Guys', 'Girls'], { is_anonymous: false });
 
-        if (poll && ctx.from) {
+        if (poll) {
+            console.log("here");
             this.pollsMap.set(poll.poll.id, {type: 'Your preference (who you want to meet)', userId: userId});
+
+            
+            console.log(this.pollsMap.get(poll.poll.id));
+
         }
 
         return poll;
@@ -37,7 +42,6 @@ export async function updateProfile(ctx: Context) {
     if (ctx.message && ctx.message.from) {
         await pollsInstance.sendGenderPoll(ctx, ctx.message.from.id);
     } else {
-        // Handle the case where ctx.message or ctx.message.from is undefined
         console.error('Context message or message.from is undefined.');
     }
 }
@@ -61,6 +65,7 @@ async function handleGenderPoll(ctx: Context, connection: any) {
     if (user) {
         user.gender = ctx.pollAnswer.option_ids[0] === 0 ? 'Male' : 'Female';
         await connection.getRepository(User).save(user);
+        console.log("WRITE SUCESS")
     }
 }
 
@@ -78,28 +83,32 @@ async function handlePreferencePoll(ctx: Context, connection: any) {
     }
 }
 
-
 export async function handleUpdateProfile(ctx: Context, connection: any) {
     if (!ctx.pollAnswer) {
         console.log("No poll answer in context.");
         return;
     }
 
-
+    console.log("HERE");
+    console.log(ctx.pollAnswer.poll_id);
     const pollInfo = pollsInstance.getPollInfo(ctx.pollAnswer.poll_id);
+
+    console.log(pollInfo);
+
     if (!pollInfo) {
         console.log("Poll info not found.");
         return;
     }
-    console.log("HERE")
+
 
     switch(pollInfo.type) {
         case 'Your Gender':
+            await handleGenderPoll(ctx, connection);
             await pollsInstance.sendPreferencePoll(ctx, pollInfo.userId);
             break;
         case 'Your preference (who you want to meet)':
-            console.log("HERE2")
-            // await pollsInstance.sendGenderPoll(ctx, pollInfo.userId);
+            await handlePreferencePoll(ctx, connection);
+            // another poll
             break;
         default:
             console.log("Unknown poll type.");
